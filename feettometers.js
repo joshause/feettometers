b598e8a8525576e648a75ecd185b75d1de90e581
Josh Ause <joshua.ause@gmail.com>
2016-08-06 15:31:36 -0700

    Strings using doublequotes.

--- feettometers.js
+++ feettometers.js
@@ -1,19 +1,19 @@
 (function() {
 
-  'use strict';
+  "use strict";
 
   var FeetToMeters = function() {};
 
   FeetToMeters.prototype.get = function(x) {
     if(typeof x !== "number") {
-      return 'invalid input';
+      return "invalid input";
     } else {
       return x * 0.3048;
     }
   };
 
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
+  if (typeof exports !== "undefined") {
+    if (typeof module !== "undefined" && module.exports) {
       exports = module.exports = FeetToMeters;
     } else {
       exports.FeetToMeters = FeetToMeters;
